04df6740ad636bd3a3e4bbdeb5d041bd2cf96338
enekogb <perreko@gmail.com>
2016-06-15 14:29:23 +0200

    Update fotocasa-listado.user.js

--- scripts/fotocasa-listado.user.js
+++ scripts/fotocasa-listado.user.js
@@ -8,38 +8,36 @@
 // ==/UserScript==
 
 // Obtenemos el jquery que usa la pagina
-var $;
+var J;
 
 // Add jQuery
-    (function(){
-        if (typeof unsafeWindow.jQuery == 'undefined') {
-            var GM_Head = document.getElementsByTagName('head')[0] || document.documentElement,
-                GM_JQ = document.createElement('script');
+(function(){
+  if (typeof unsafeWindow.jQuery == 'undefined') {
+    var GM_Head = document.getElementsByTagName('head')[0] || document.documentElement,
+    GM_JQ = document.createElement('script');
     
-            GM_JQ.src = 'http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js';
-            GM_JQ.type = 'text/javascript';
-            GM_JQ.async = true;
+    GM_JQ.src = 'http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js';
+    GM_JQ.type = 'text/javascript';
+    GM_JQ.async = true;
     
-            GM_Head.insertBefore(GM_JQ, GM_Head.firstChild);
-        }
-        GM_wait();
-    })();
+    GM_Head.insertBefore(GM_JQ, GM_Head.firstChild);
+  }
+  GM_wait();
+})();
 
 // Check if jQuery's loaded
-    function GM_wait() {
-        if (typeof unsafeWindow.jQuery == 'undefined') {
-            window.setTimeout(GM_wait, 100);
-        } else {
-            $ = unsafeWindow.jQuery.noConflict(true);
-            letsJQuery();
-        }
-    }
+function GM_wait() {
+  if (typeof unsafeWindow.jQuery == 'undefined') {
+    window.setTimeout(GM_wait, 100);
+  } else {
+    J = unsafeWindow.jQuery.noConflict(true);
+    letsJQuery();
+  }
+}
 
 // All your GM code must be inside this function
-    function letsJQuery() {
-        console.log('Ejecutando userscript...');
-        console.log($('#search-listing'));
-        console.log('Ejecutado userscript...');
-        console.log($().jquery); // check jQuery version
-    }
-  
+function letsJQuery() {
+  console.log('Ejecutando userscript...');
+  console.log(J('#search-listing'));
+  console.log(J().jquery); // check jQuery version
+}
