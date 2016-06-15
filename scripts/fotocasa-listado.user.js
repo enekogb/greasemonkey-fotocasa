fa17775990bff76c80625b2c054bfe02cbc184e2
enekogb <perreko@gmail.com>
2016-06-15 14:00:46 +0200

    Update fotocasa-listado.user.js

--- scripts/fotocasa-listado.user.js
+++ scripts/fotocasa-listado.user.js
@@ -12,4 +12,9 @@ var $, jQuery;
 $ = jQuery = window.jQuery;
 
 // Annadimos el css de FontAwesome a la pagina
-$("head link[rel='stylesheet']").last().after("<link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css' type='text/css' media='screen'>");
+// $("head link[rel='stylesheet']").last().after("<link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css' type='text/css' media='screen'>");
+
+// Buscamos las filas del listado de resultados
+$('#search-listing').find('tr [data-adid]').each(function(index) {
+  console.log($(this));
+});
